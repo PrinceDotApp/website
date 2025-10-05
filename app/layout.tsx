35119c9d6c8a9b6fdec53c954c7ceb0dc2fd3058
Neil Shah <neil@bronze.app>
2025-10-05 05:15:31 -0400

    Fix home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Navbar } from '../components/navbar';
 import { Footer } from '@/components/footer';
 import { Toaster } from 'sonner';
 import './globals.css';
@@ -22,7 +21,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="dark">
       <body className={`${inter.variable} bg-background font-sans antialiased`}>
         <div className="min-h-screen max-w-4xl mx-auto px-4">
-          <Navbar />
           <div className="py-4">{children}</div>
           <div className="sticky top-[100vh] pt-32 pb-4">
             <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main className="w-full text-center px-4">
+    <main className="w-full min-h-screen flex items-center justify-center text-center px-4">
       <div className="max-w-4xl mx-auto">
-        <p className="mt-6 font-medium text-lg md:text-xl text-white tracking-tight max-w-2xl mx-auto">
+        <p className="font-medium text-sm text-white tracking-tight mx-auto">
           hello@prince.app
         </p>
       </div>
